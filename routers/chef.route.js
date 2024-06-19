d40a26bd5a51be5fdfbd666eb6837b2adcf3ae46
Masi-92 <masoud.izadi@92gmali.com>
2024-06-19 12:44:53 +0200

    crade/chef/rote

--- routers/chef.route.js
+++ routers/chef.route.js
@@ -8,3 +8,9 @@ import { ROLES } from "../models/user.model.js";
 const route = Router();
 route.post("/createChef", auth, hasRole(ROLES.SUPER_ADMIN), createChef);
 route.put("/updateChef/:id", auth, hasRole(ROLES.SUPER_ADMIN), updateChef);
+route.delete("/deleteChef/:id", auth, hasRole(ROLES.SUPER_ADMIN), deleteChef);
+route.get("/getChef", auth, hasRole(ROLES.SUPER_ADMIN), getChef);
+route.get("/getChefById/:id", auth, hasRole(ROLES.SUPER_ADMIN), getChefById);
+
+
+export default route;
