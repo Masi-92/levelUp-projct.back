5ac632a47059971fdd39146bd960ecb060e91a80
Masi-92 <masoud.izadi@92gmali.com>
2024-06-19 12:39:31 +0200

    middelware/auth

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+import Jwt from "jsonwebtoken";
+
+
+
+export const auth = (req, res, next) => {
+  try {
+    const token = req.headers.token;
+    const decode = Jwt.verify(token, process.env.SECRET);
+
+    req.user = decode;
+    next();
+  } catch (error) {
+    res.status(401).send({ message: "access denied" });
+  }
+};
+
+
+
