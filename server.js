afbc5d8c97deeaac357635e17721b66cf342ee63
Masi-92 <masoud.izadi@92gmali.com>
2024-06-19 12:49:35 +0200

    sever/i/cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-
+app.use(cors());
 app.listen(PORT, () => {
   console.log(`Server is listening on port:${PORT}`);
 });
