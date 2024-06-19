1c5c079fc730002ba6eff1f073e1296d6ccdeb8a
Masi-92 <masoud.izadi@92gmali.com>
2024-06-19 12:40:33 +0200

    midd/role

---
+++ middleware/role.js
@@ -0,0 +1,10 @@
+export const hasRole =
+  (...roles) =>
+  (req, res, next) => {
+    try {
+      if (!roles.includes(req.user.role)) throw new Error();
+      next();
+    } catch (error) {
+      res.status(403).send({ message: "access denied" });
+    }
+  };
