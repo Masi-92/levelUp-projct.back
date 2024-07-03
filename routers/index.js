764eb15154fc8dcf8128c02d9760878b3023c04e
Masi-92 <masoud.izadi@92gmali.com>
2024-07-03 20:56:18 +0200

    index

--- routers/index.js
+++ routers/index.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
 import userRoute from "./user.route.js";
-import chefRoute from "./chef.route.js"
+import chefRoute from "./chef.route.js";
+import coachRoute from "./coach.route.js";
+import clientRoute from "./client.route.js";
+import messageRoute from "./message.route.js";
+import todoRoute from "./todo.route.js";
+import notificationRoute from "./notification.route.js";
 
 const router = Router();
 
 router.use("/user", userRoute);
-
 router.use("/chef", chefRoute);
+router.use("/coach", coachRoute);
+router.use("/client", clientRoute);
+router.use("/message", messageRoute);
+router.use("/todo", todoRoute);
+router.use("/notification", notificationRoute);
 
 
 
