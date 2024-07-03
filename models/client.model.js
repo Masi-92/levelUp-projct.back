829ea0e212013ded023844a3b3c8c8c445c656c3
Masi-92 <masoud.izadi@92gmali.com>
2024-07-03 20:44:02 +0200

    CAR_LICENSE

--- models/client.model.js
+++ models/client.model.js
@@ -5,3 +5,23 @@ const GENDER = {
     MALE: "male",
     OTHER: "other",
   };
+  const CAR_LICENSE = {
+    AM: "moped",
+    A1: "leichtkraftrad",
+    A2: "mittelstarkes_motorrad",
+    A: "motorrad",
+    B: "pkw",
+    BE: "pkw_mit_anhaenger",
+    C1: "leichter_lkw",
+    C1E: "leichter_lkw_mit_anhaenger",
+    C: "lkw",
+    CE: "lkw_mit_anhaenger",
+    D1: "kleinbus",
+    D1E: "kleinbus_mit_anhaenger",
+    D: "bus",
+    DE: "bus_mit_anhaenger",
+    L: "traktor",
+    T: "traktor_und_forstwirtschaftliche_fahrzeuge",
+    F: "landwirtschaftliche_fahrzeuge",
+  };
+  
