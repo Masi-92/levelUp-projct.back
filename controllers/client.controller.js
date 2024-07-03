2fc32952e0000f6104b086a77a70dd8ebd74bb71
Masi-92 <masoud.izadi@92gmali.com>
2024-07-03 20:48:26 +0200

    createClient

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,2 +1,8 @@
 import clientModel from "../models/client.model.js";
 
+export const createClient = async (req, res) => {
+    if (user) return res.status(400).send({ message: "client with username already exist!" });
+    const client = await clientModel.create(req.body);
+  
+    res.send(client);
+  };
