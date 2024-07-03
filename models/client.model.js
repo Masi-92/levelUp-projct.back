9bd1245e94037507705224b28de53d2e814cb1c1
Masi-92 <masoud.izadi@92gmali.com>
2024-07-03 20:46:55 +0200

    docSchema

--- models/client.model.js
+++ models/client.model.js
@@ -81,3 +81,150 @@ const GENDER = {
     FRIENDS: "friends",
     OTHER: "other",
   };
+
+  const docSchema = new Schema({
+    name: String,
+    file: String,
+  });
+
+  const docSchema = new Schema({
+    name: String,
+    file: String,
+  });
+  
+  const schema = new Schema(
+    {
+      gender: { type: String, enum: [GENDER.FEMALE, GENDER.MALE, GENDER.OTHER] },
+      firstName: String,
+      lastName: String,
+      clientNumber: String,
+      addressStr: String,
+      addressDescription: String,
+      zipCode: String,
+      city: String,
+      state: String,
+      land: String,
+      phoneNumber: String,
+      houseTel: String,
+      birthDay: Date,
+      email: String,
+      bewertung: Number,
+      angebot: String,
+      status: { type: String, enum: [STATUS.IN_ARBEIT, STATUS.IN_PROGRESS, STATUS.RESERVE] },
+      branch: String,
+      knownFromWhere: {
+        type: String,
+        enum: [KNOWN_FROM_WHERE.FACEBOOK, KNOWN_FROM_WHERE.FRIENDS, KNOWN_FROM_WHERE.INSTAGRAM, KNOWN_FROM_WHERE.OTHER],
+      },
+      owner: {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+      creator: {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+      firstCallDate: Date,
+  
+      birthPlace: String,
+      nationality: String,
+      migration: Boolean,
+      insurance: Boolean,
+      workPermission: { type: String, enum: [WorkPermission.YES, WorkPermission.NO, WorkPermission.SOON] },
+      marital: { type: String, enum: [MARITAL_STATUS.DIVORCED, MARITAL_STATUS.MARRIED, MARITAL_STATUS.SINGLE] },
+      children: Number,
+      personnelCode: String,
+      personnelCodeExpire: Date,
+      reha: Boolean,
+      workTimeStatus: {
+        type: String,
+        enum: [WORK_TIME_STATUS.FULL_TIME, WORK_TIME_STATUS.HALF_TIME, WORK_TIME_STATUS.PART_TIME],
+      },
+      disabilityPercent: Number,
+      disabilityType: {
+        type: String,
+        enum: [DISABILITY_TYPE.ADDICT, DISABILITY_TYPE.FAMILY, DISABILITY_TYPE.MOTIVATION, DISABILITY_TYPE.SICKNESS],
+      },
+      disabilityDescription: String,
+  
+      skill: [String],
+      arbeitLos: Boolean,
+      arbeitLosFromDate: Date,
+      arbeitFromDate: Date,
+      // todo
+      kundigungsFrist: {
+        type: String,
+        enum: [
+          KUNDIGUNGS_STATUS.ONE_MONTH,
+          KUNDIGUNGS_STATUS.ONE_WEEK,
+          KUNDIGUNGS_STATUS.TWO_WEEK,
+          KUNDIGUNGS_STATUS.SOON,
+        ],
+      },
+      kundigungsDescription: String,
+      scheinStatus: {
+        type: String,
+        enum: [SCHEIN_STATUS.ALG1, SCHEIN_STATUS.ALG2, SCHEIN_STATUS.NONE, SCHEIN_STATUS.SOCIAL],
+      },
+      jobCenterNumber: String,
+      jobCenterClientNumber: String,
+      jobCenterTeam: String,
+      jobCenterCoach: String,
+      jobCenterTel: String,
+      jobCenterEmail: String,
+      jobCenterMobile: String,
+  
+      jobPlaceDistance: Number,
+      moving: Boolean,
+      internEinsatz: Boolean,
+      wholeCountry: [String],
+      jobPlaceDescription: [String],
+  
+      favoriteJob: [String],
+      favoriteJobDescription: String,
+      privateComment: String,
+      secretorJobOffer: String,
+  
+      hasPkw: Boolean,
+      pkwLicense: [
+        {
+          type: String,
+          enum: [
+            CAR_LICENSE.AM,
+            CAR_LICENSE.A1,
+            CAR_LICENSE.A2,
+            CAR_LICENSE.A,
+            CAR_LICENSE.B,
+            CAR_LICENSE.BE,
+            CAR_LICENSE.C1,
+            CAR_LICENSE.C1E,
+            CAR_LICENSE.C,
+            CAR_LICENSE.CE,
+            CAR_LICENSE.D1,
+            CAR_LICENSE.D1E,
+            CAR_LICENSE.D,
+            CAR_LICENSE.DE,
+            CAR_LICENSE.L,
+            CAR_LICENSE.T,
+            CAR_LICENSE.F,
+          ],
+        },
+      ],
+  
+      resumeDoc: String,
+      picture: String,
+      docs: [docSchema],
+  
+      isDeleted: { type: Boolean, default: false },
+  
+      // lang: String,
+      // jobPermission: Boolean,
+      // carLicense: { type: String, enum: [CAR_LICENSE.MOTOR] },
+      // description: String,
+      // education: String,
+    },
+    { timestamps: true }
+  );
+  
+  export default model("client", schema);
+  
