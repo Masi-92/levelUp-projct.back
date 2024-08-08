35f45722c529686ff0f5d033a2f1b510e06b8075
Masi-92 <masoud.izadi@92gmali.com>
2024-08-08 09:34:32 +0200

    model/termin

--- models/termin.model.js
+++ models/termin.model.js
@@ -1,16 +1,45 @@
 import { model, Schema } from "mongoose";
 
-const schema = new Schema({
-  coach: {
-    type: Schema.Types.ObjectId,
-    ref: "user",
+const TerminStatus = {
+  PENDING: "pending",
+  COMPLETED: "completed",
+  CANCELED: "canceled",
+  UPDATE_TIME: "updateTime",
+};
+
+const schema = new Schema(
+  {
+    coach: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    client: {
+      type: Schema.Types.ObjectId,
+      ref: "client",
+    },
+    date: { type: Date, required: true },
+    subject: { type: String, required: true },
+    description: String,
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: [TerminStatus.PENDING, TerminStatus.COMPLETED, TerminStatus.UPDATE_TIME, TerminStatus.CANCELED],
+      default: TerminStatus.PENDING,
+    },
+    endTime: Date,
+    delay: Number,
+    meetingTime: Number,
   },
-  client: {
-    type: Schema.Types.ObjectId,
-    ref: "client",
-  },
-  date: Date,
-  subject: String,
-});
+  { timestamps: true }
+);
 
 export default model("termin", schema);
+
+
+
