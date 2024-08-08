60f85c2923fe9e96b32044bb30ded2a314b0323d
Masi-92 <masoud.izadi@92gmali.com>
2024-08-08 09:25:35 +0200

    note/controller

---
+++ controllers/note.controller.js
@@ -0,0 +1,24 @@
+import noteModel from "../models/note.model.js";
+
+export const createNote = async (req, res) => {
+  const userId = req.user.id;
+
+  const note = await noteModel.create({ ...req.body, coach: userId });
+
+  res.send(note);
+};
+
+export const deleteNote = async (req, res) => {
+  const { id } = req.params;
+  const note = await noteModel.findByIdAndDelete(id);
+  if (!note) return res.status(404).send({ message: "note not found" });
+  res.send(note);
+};
+
+export const getNote = async (req, res) => {
+  const { clientId } = req.params;
+  const notes = await noteModel.find({
+    client: clientId,
+  }).sort({createdAt : -1});
+  res.send(notes);
+};
