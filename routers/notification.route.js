5528e39fcd451f3ed452450ec208d1d04db65629
Masi-92 <masoud.izadi@92gmali.com>
2024-08-08 09:46:52 +0200

    noti/router

--- routers/notification.route.js
+++ routers/notification.route.js
@@ -1,15 +1,10 @@
 import { Router } from "express";
-import {
-    getNotification,
-    seenNotification
-} from "../controllers/notification.controller.js";
+import { getNotification, seenNotification } from "../controllers/notification.controller.js";
 import { auth } from "../middleware/auth.js";
-import { hasRole } from "../middleware/role.js";
-import { ROLES } from "../models/user.model.js";
 
 const route = Router();
 
-route.put("/seenNotification/:id", auth, hasRole(ROLES.COACH,ROLES.SECRETARY), seenNotification);
-route.get("/getNotification", auth, hasRole(ROLES.COACH,ROLES.SECRETARY), getNotification);
+route.put("/seenNotification/:id", auth, seenNotification);
+route.get("/getNotification", auth, getNotification);
 
 export default route;
