0252b71676a1cec4f884f785ee6f8a200634bbb4
Masi-92 <masoud.izadi@92gmali.com>
2024-08-08 09:29:24 +0200

    user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,25 +3,34 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const login = async (req, res) => {
-    const { username, password } = req.body;
-  
-    if (!username || !password) return res.status(400).send({ message: "username or password is empty" });
-  
-    const user = await userModel.findOne({ username });
-    if (!user) return res.status(400).send({ message: "username or password is incorrect" });
-  
-    const isMatched = await bcrypt.compare(password, user.password);
-    if (!isMatched) return res.status(400).send({ message: "username or password is incorrect" });
-  
-    const token = jwt.sign(
-      {
-        id: user._id,
-        role: user.role,
-      },
-      process.env.SECRET,
-      { expiresIn: "3d" }
-    );
-  
-    res.send({ token, fullName: user.fullName, image: user.image });
-  };
-  
+  const { username, password } = req.body;
+
+  if (!username || !password) return res.status(400).send({ message: "username or password is empty" });
+
+  const user = await userModel.findOne({ username });
+  if (!user) return res.status(400).send({ message: "username or password is incorrect" });
+
+  const isMatched = await bcrypt.compare(password, user.password);
+  if (!isMatched) return res.status(400).send({ message: "username or password is incorrect" });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      role: user.role,
+    },
+    process.env.SECRET,
+    { expiresIn: "3d" }
+  );
+
+  res.send({ token, fullName: user.fullName, image: user.image });
+};
+
+export const updateProfile = async (req, res) => {
+  const userId = req.user.id;
+
+  const result = await userModel.findByIdAndUpdate(userId, {
+    $set: req.body,
+  },{new:true});
+
+  res.send(result)
+};
