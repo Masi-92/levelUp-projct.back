005f9fde2615a0ec84fa2c5029847bf7680db59c
Masi-92 <masoud.izadi@92gmali.com>
2024-09-10 11:16:18 +0200

    noti_con

--- controllers/notification.controller.js
+++ controllers/notification.controller.js
@@ -1,40 +1,39 @@
 import notificationModel from "../models/notification.model.js";
 
-
 export const createNotification = async ({ text, type, subjectId, action, user, creator }) => {
-    const notification = await notificationModel.create({
-      text,
-      type,
-      action,
-      user,
-      creator,
-      subjectId,
-    });
-  
-    return notification;
-  };
-  
+  const notification = await notificationModel.create({
+    text,
+    type,
+    action,
+    user,
+    creator,
+    subjectId,
+  });
 
-  export const seenNotification = async (req, res) => {
-    const { id } = req.params;
-  
-    const updateBody = {
-      seen: true,
-    };
+  return notification;
+};
 
-    const notification = await notificationModel.findByIdAndUpdate(id, { $set: updateBody }, { new: true });
-    if (!notification) return res.status(404).send({ message: "notification not found" });
-    res.send(notification);
+export const seenNotification = async (req, res) => {
+  const { id } = req.params;
+
+  const updateBody = {
+    seen: true,
   };
 
-  export const getNotification = async (req, res) => {
-    const userId = req.user.id;
-  
-    const notifications = await notificationModel
-      .find({
-        user: userId,
-      })
-      .populate("creator", "image fullName username");
-    res.send(notifications);
-  };
-  
+  const notification = await notificationModel.findByIdAndUpdate(id, { $set: updateBody }, { new: true });
+  if (!notification) return res.status(404).send({ message: "Benachrichtigung nicht gefunden" });
+  res.send(notification);
+};
+
+export const getNotification = async (req, res) => {
+  const userId = req.user.id;
+
+  const notifications = await notificationModel
+    .find({
+      user: userId,
+    })
+    .sort({_id:-1})
+    .limit(10)
+    .populate("creator", "image fullName username");
+  res.send(notifications);
+};
