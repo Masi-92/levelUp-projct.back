4f0dad00d08acdd4ba46ce17b83b405494a80da2
Masi-92 <masoud.izadi@92gmali.com>
2024-09-12 11:18:05 +0200

    todo.route

--- routers/todo.route.js
+++ routers/todo.route.js
@@ -1,15 +1,16 @@
 import { Router } from "express";
-import { createTodo, deleteTodo, getTodo, getTodoById, updateTodo } from "../controllers/todo.controller.js";
+import { createTodo, deleteTodo, getCoachTodoByChef, getTodo, getTodoById, updateTodo } from "../controllers/todo.controller.js";
 import { auth } from "../middleware/auth.js";
 import { hasRole } from "../middleware/role.js";
 import { ROLES } from "../models/user.model.js";
 
 const route = Router();
 
-route.post("/createTodo", auth, hasRole(ROLES.COACH,ROLES.SECRETARY), createTodo);
-route.put("/updateTodo/:id", auth, hasRole(ROLES.COACH,ROLES.SECRETARY), updateTodo);
-route.delete("/deleteTodo/:id", auth, hasRole(ROLES.COACH,ROLES.SECRETARY), deleteTodo);
-route.get("/getTodo", auth, hasRole(ROLES.COACH,ROLES.SECRETARY), getTodo);
-route.get("/getTodoById/:id", auth, hasRole(ROLES.COACH,ROLES.SECRETARY) , getTodoById);
+route.post("/createTodo", auth, createTodo);
+route.put("/updateTodo/:id", auth, updateTodo);
+route.delete("/deleteTodo/:id", auth, deleteTodo);
+route.get("/getTodo", auth, getTodo);
+route.get("/getCoachTodoByChef", auth,hasRole(ROLES.CHEF), getCoachTodoByChef);
+route.get("/getTodoById/:id", auth, getTodoById);
 
 export default route;
