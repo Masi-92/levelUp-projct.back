3891ecdd3dd81864aeec058eab8c3ac4d35d3d15
Masi-92 <masoud.izadi@92gmali.com>
2024-09-12 11:18:43 +0200

    termin.sch

--- validation/termin.schema.js
+++ validation/termin.schema.js
@@ -5,6 +5,7 @@ export const createTerminSchema = Joi.object({
   subject: Joi.string().required(),
   coach: Joi.objectId().required(),
   client: Joi.objectId(),
+  clientName: Joi.string().allow(""),
   date: Joi.string().required(),
 });
 
