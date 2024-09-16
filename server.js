7dba3e586d8a6c440d84b4083e85145d7c90e00d
Masi-92 <masoud.izadi@92gmali.com>
2024-09-16 13:05:56 +0200

    server

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 import cors from 'cors';
 import dotenv from "dotenv";
 import express from "express";
+import { fileURLToPath } from 'url';
 import routes from "./routers/index.js";
+import path from 'path'
 import "./utils/mongodb.js";
-import {createSuperAdmin} from "./seed.js" 
-dotenv.config();
 
-const PORT = process.env.PORT;
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT  ; 
 const DB_URL = process.env.DB_URL;
 const app = express();
 
 
 app.use("/uploads",express.static("uploads"));
+app.use(express.static(__dirname + "/client"))
 app.use(express.json());
 app.use(cors());
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port:${PORT}`);
@@ -21,4 +29,7 @@ app.listen(PORT, () => {
 
 app.use("/api", routes);
 
+app.get("/*", (req, res) => {
+  res.sendFile(__dirname + "/client/index.html");
+});
  //createSuperAdmin()
