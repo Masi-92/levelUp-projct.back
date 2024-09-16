c9e3806ccd727038cf8ec7c823925d6e4ac330b5
Masi-92 <masoud.izadi@92gmali.com>
2024-09-16 12:56:02 +0200

    client

--- routers/client.route.js
+++ routers/client.route.js
@@ -1,15 +1,24 @@
 import { Router } from "express";
-import { createClient, deleteClient, getClient, getClientById, updateClient } from "../controllers/client.controller.js";
+import {
+    createClient,
+    deleteClient,
+    getClient,
+    getClientById,
+    updateClient,
+    updateClientOwner,
+} from "../controllers/client.controller.js";
 import { auth } from "../middleware/auth.js";
-import { hasRole } from "../middleware/role.js";
-import { ROLES } from "../models/user.model.js";
+import { validate } from "../middleware/validate.middleware.js";
+import { createClientSchema } from "../validation/client.schema.js";
 
 const route = Router();
 
-route.post("/createClient", auth, hasRole(ROLES.SECRETARY,ROLES.COACH), createClient);
-route.put("/updateClient/:id", auth, hasRole(ROLES.SECRETARY,ROLES.COACH), updateClient);
-route.delete("/deleteClient/:id", auth, hasRole(ROLES.SECRETARY,ROLES.COACH), deleteClient);
-route.get("/getClient", auth, hasRole(ROLES.SECRETARY,ROLES.COACH), getClient);
-route.get("/getClientById/:id", auth, hasRole(ROLES.SECRETARY,ROLES.COACH), getClientById);
+route.post("/createClient", auth, validate(createClientSchema), createClient);
+route.put("/updateClient/:id", auth, updateClient);
+route.put("/updateClientOwner/:id", auth, updateClientOwner);
+route.delete("/deleteClient/:id", auth, deleteClient);
+route.get("/getClient", auth, getClient);
+route.get("/getClientById/:id", auth, getClientById);
 
 export default route;
+
