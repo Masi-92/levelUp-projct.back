107cea97b12f631168cc75aa469cca945f0059a6
Masi-92 <masoud.izadi@92gmali.com>
2024-09-24 22:05:37 +0200

    mongo

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import {createSuperAdmin }  from  "./../seed.js"
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URL, { dbName: process.env.DATABASE })
   .then(() => {
- //createSuperAdmin()
+    //createSuperAdmin()
+
     return console.log("Connected to mongoDB");
   })
+  .catch(() => console.log("Connection error"));
 
-  .catch((error) => console.log("Connection error: ", error.message));
+  
