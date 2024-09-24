0684a91356bbdf7818d0d35918ed3cd618ef3ef8
Masi-92 <masoud.izadi@92gmali.com>
2024-09-24 22:01:06 +0200

    termin.route.js

--- routers/termin.route.js
+++ routers/termin.route.js
@@ -9,13 +9,14 @@ import {
 import { auth } from "../middleware/auth.js";
 import { validate } from "../middleware/validate.middleware.js";
 import { createTerminSchema } from "../validation/termin.schema.js";
+import { paginate } from "../middleware/paginate.middleware.js";
 
 const route = Router();
 
 route.post("/createTermin", auth, validate(createTerminSchema), createTermin);
 route.put("/updateTermin/:id", auth, updateTermin);
 route.delete("/deleteTermin/:id", auth, deleteTermin);
-route.get("/getTermin", auth, getTermin);
+route.get("/getTermin", auth,paginate, getTermin);
 route.get("/getTerminById/:id", auth, getTerminById);
 
 export default route;
