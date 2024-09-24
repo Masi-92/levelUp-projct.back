512c0eac0eae1362b62928e2d8d779a534addcbc
Masi-92 <masoud.izadi@92gmali.com>
2024-09-24 22:05:23 +0200

    todo/controller

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -5,7 +5,7 @@ import { createNotification } from "./notification.controller.js";
 
 export const createTodo = async (req, res) => {
   const { id: userId, role } = req.user;
-  const { text, coach } = req.body;
+  const { text, coach, date } = req.body;
 
   if ((role === ROLES.COACH || role === ROLES.SECRETARY) && coach)
     return res.status(400).send({ message: "you can not create todo to other coaches" });
@@ -14,6 +14,7 @@ export const createTodo = async (req, res) => {
     text,
     user: coach || userId,
     creator: userId,
+    date,
   });
 
   createNotification({
@@ -30,10 +31,11 @@ export const createTodo = async (req, res) => {
 
 export const updateTodo = async (req, res) => {
   const { id } = req.params;
-  const { text, done } = req.body;
+  const { text, done, date } = req.body;
 
   const updateBody = {};
   if (text) updateBody.text = text;
+  if (date) updateBody.date = date;
   if (done !== undefined) updateBody.done = done;
   const todo = await todoModel.findByIdAndUpdate(id, { $set: updateBody }, { new: true });
   if (!todo) return res.status(404).send({ message: "todo nicht gefunden" });
@@ -50,16 +52,19 @@ export const deleteTodo = async (req, res) => {
 export const getTodo = async (req, res) => {
   const userId = req.user.id;
 
-  const search = req.query.search || "";
-  const personal = req.query.personal === "true";
+  const byChef = req.query.by === "chef";
+  const byOwn = req.query.by === "own";
+  const { search = "", from, to } = req.query;
   const searchRegex = new RegExp(search, "i");
   const findQuery = {
     user: userId,
     text: { $regex: searchRegex },
   };
-  if (personal) findQuery.creator = userId;
-  else findQuery.creator = {$ne : userId};
-  const todos = await todoModel.find(findQuery).sort({createdAt : -1});
+  if (byOwn) findQuery.creator = userId;
+  else if (byChef) findQuery.creator = { $ne: userId };
+  if (from && to) findQuery.date = { $gte: new Date(from), $lt: new Date(to) };
+
+  const todos = await todoModel.find(findQuery).sort({ date: -1, createdAt: -1 });
   res.send(todos);
 };
 
@@ -67,13 +72,16 @@ export const getCoachTodoByChef = async (req, res) => {
   const userId = req.user.id;
   const search = req.query.search || "";
   const searchRegex = new RegExp(search, "i");
-  const todos = await todoModel.find({
-    creator: userId,
-    user: {
-      $ne: userId,
-    },
-    text: { $regex: searchRegex },
-  }).populate("user","fullName username").sort({createdAt : -1});
+  const todos = await todoModel
+    .find({
+      creator: userId,
+      user: {
+        $ne: userId,
+      },
+      text: { $regex: searchRegex },
+    })
+    .populate("user", "fullName username")
+    .sort({ createdAt: -1 });
   res.send(todos);
 };
 export const getTodoById = async (req, res) => {
